794fff4120fc3a4e2075f2f788c270ce2ebada7a
JaznanOfficial <jaznanofficial@gmail.com>
2024-11-28 11:50:23 +0600

    services page done

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,25 +1,33 @@
 import Image from "next/image";
+import saasImage from "../public/assets/services/saas.png";
+import softwareImage from "../public/assets/services/software.png";
+import uiImage from "../public/assets/services/ui.png";
+import partnerImage from "../public/assets/services/partner.png";
 
 const services = [
   {
-    icon: "assets/services/icon-1.svg",
-    title: "UI UX Design",
-    description: "Designing compelling engaging user experiences.",
+    icon: saasImage,
+    title: "Complete SAAS Business Develpment",
+    description:
+      "We provide end-to-end SaaS business development services, from conceptualization and prototyping to launch and scaling. Our expertise includes building secure, scalable architectures, implementing seamless user experiences, and integrating cutting-edge features to help your business grow and stay competitive in the SaaS industry.",
   },
   {
-    icon: "assets/services/icon-2.svg",
-    title: "Web Development",
-    description: "Developing robust, scalable websites for all devices.",
+    icon: softwareImage,
+    title: "Customized Software Solutions",
+    description:
+      "Our team specializes in creating tailored software solutions that perfectly align with your business goals. Whether you need a custom CRM, ERP, or a full-scale enterprise application, we build robust and flexible systems that enhance efficiency, productivity, and scalability.",
   },
   {
-    icon: "assets/services/icon-3.svg",
-    title: "E-commerce Solutions",
-    description: "Building secure, user-friendly online stores to drive sales.",
+    icon: uiImage,
+    title: "UI/UX and Logo Design",
+    description:
+      "We craft beautiful and user-friendly UI/UX designs that elevate your brand and deliver a seamless experience. From user journey mapping to wireframing and high-fidelity prototyping, our design process ensures your product not only looks great but also functions flawlessly. We also create memorable logos that represent your brand identity.",
   },
   {
-    icon: "assets/services/icon-4.svg",
-    title: "Care & Support",
-    description: "Providing updates, security and support for performance",
+    icon: partnerImage,
+    title: "Game Changing Business Support, Partnership and Collaboration",
+    description:
+      "We offer strategic partnerships and collaborations that leverage digital technologies to transform your business operations. Our services provide innovative digital solutions to optimize workflows, improve customer engagement, and enhance overall business performance, creating long-term value and growth.",
   },
 ];
 
@@ -27,7 +35,7 @@ const Services = () => {
   return (
     <section className=" z-40">
       <div className="container mx-auto">
-        <ul className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-[20px] -top-12 place-items-center lg:place-items-stretch">
+        <ul className=" grid grid-cols-1 md:grid-cols-2 gap-[20px] -top-12 place-items-center lg:place-items-stretch">
           {services.map((service, index) => {
             return (
               <li
@@ -36,8 +44,8 @@ const Services = () => {
               >
                 <Image
                   src={service.icon}
-                  width={48}
-                  height={48}
+                  width={76}
+                  height={76}
                   alt=""
                   className="mb-4"
                 />
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: "img.icons8.com",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
